274f175b391fc8625c5c575834c7e31924e3eccf
trikitrok <trikitrok@gmail.com>
2014-05-02 21:41:00 +0200

    Increased duplication a bit before extracting a method

--- src/GildedRose.java
+++ src/GildedRose.java
@@ -18,8 +18,12 @@ public class GildedRose {
         if (isPerishable(item)) {
             decrementQuality(item);
         } else {
+
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
+            }
+
+            if (item.quality < 50) {
 
                 if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                     if (item.sellIn < 11) {
