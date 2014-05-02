d057c322c4459284bb84d40f7809d836657b91a6
trikitrok <trikitrok@gmail.com>
2014-05-02 21:35:02 +0200

    Added guard clause for Sulfuras

--- src/GildedRose.java
+++ src/GildedRose.java
@@ -12,12 +12,13 @@ public class GildedRose {
     }
 
     private void degrade(Item item) {
+        if (item.name.equals("Sulfuras, Hand of Ragnaros"))
+            return;
+
         if (!item.name.equals("Aged Brie")
                 && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
             if (item.quality > 0) {
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    item.quality = item.quality - 1;
-                }
+                item.quality = item.quality - 1;
             }
         } else {
             if (item.quality < 50) {
@@ -39,17 +40,13 @@ public class GildedRose {
             }
         }
 
-        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            item.sellIn = item.sellIn - 1;
-        }
+        item.sellIn = item.sellIn - 1;
 
         if (item.sellIn < 0) {
             if (!item.name.equals("Aged Brie")) {
                 if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                     if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            item.quality = item.quality - 1;
-                        }
+                        item.quality = item.quality - 1;
                     }
                 } else {
                     item.quality = item.quality - item.quality;
