063e280702ab150f70776573cf6a4a04f1f7e91d
trikitrok <trikitrok@gmail.com>
2014-05-04 19:46:50 +0200

    Extracted some helpe methods

--- src/DegradableItems.java
+++ src/DegradableItems.java
@@ -16,12 +16,19 @@ public class DegradableItems {
     }
 
     public static DegradableItems createFrom(Item[] items) {
+        return new DegradableItems(selectDegradable(items));
+    }
+
+    private static List<DegradableItem> selectDegradable(Item[] items) {
         List<DegradableItem> degradableItems = new ArrayList<DegradableItem>();
         for (Item item : items) {
-            if (item.name.contains("Sulfuras, Hand of Ragnaros"))
-                continue;
-            degradableItems.add(DegradableItem.createFrom(item));
+            if (!isImmutable(item))
+                degradableItems.add(DegradableItem.createFrom(item));
         }
-        return new DegradableItems(degradableItems);
+        return degradableItems;
+    }
+
+    private static boolean isImmutable(Item item) {
+        return item.name.contains("Sulfuras, Hand of Ragnaros");
     }
 }
